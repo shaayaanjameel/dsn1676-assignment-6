32948d228a8cc6db2d4f3bc7f2495e986b2f1a9d
Shaayaan Jameel <Shaayaan@Shaayaans-MacBook-Pro.local>
2015-03-11 23:03:55 -0400

    added js variables

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var $btnShow = $('.btn-show-hide');
+var $box = $('.box');
+
+var $btnMove = $('.btn-move');
+var $diamond = $('.diamond');
+
+var $btnCollapse = $('.btn-collapse-expand');
+var $panel = $('.panel');
+
+var $btnBounce = $('.btn-bounce');
+var $circle = $('.circle');
+
+var $btnAppend = $('.btn-append');
+var $list = $('.list');
